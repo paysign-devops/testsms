6c52cc5b23c79c7d8380a8ceceed47d78ef15acd
Paysign_DevOps <68657347+paysign-devops@users.noreply.github.com>
2020-07-31 15:57:58 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 80
 
-app.get('/', (req, res) => res.send('Lets see if this works after removing the Secrets from Github'))
+app.get('/', (req, res) => res.send('All works well. This is the Hello, World app!'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
