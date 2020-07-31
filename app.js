9e88ce38979d8f914beb192230eb02cef70008de
Paysign_DevOps <68657347+paysign-devops@users.noreply.github.com>
2020-07-31 13:10:10 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 80
 
-app.get('/', (req, res) => res.send('This is a test! If you can read this message on Port 80, it WORKED!'))
+app.get('/', (req, res) => res.send('Hello, World'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
