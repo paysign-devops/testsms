9200604673ccd78114700cef504e0426114f85e3
Paysign_DevOps <68657347+paysign-devops@users.noreply.github.com>
2020-07-31 14:02:26 -0700

    removed Secrets from Github

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 80
 
-app.get('/', (req, res) => res.send('Hello, From Inside Of The World'))
+app.get('/', (req, res) => res.send('Lets see if this works after removing the Secrets from Github'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
