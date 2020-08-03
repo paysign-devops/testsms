c953b71f9971cb5effff79ce63921df7c3dd0253
Paysign_DevOps <68657347+paysign-devops@users.noreply.github.com>
2020-08-03 09:36:23 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ pconst express = require('express')
 const app = express()
 const port = 80
 
-app.get('/', (req, res) => res.send('If you can see this, the pipeline is working from Github to AWS ECR'))
+app.get('/', (req, res) => res.send('If you can see this, the pipeline is working'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
