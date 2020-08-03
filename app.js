20cb55ecd4b488d317fe6b3069ad414e92385695
Paysign_DevOps <68657347+paysign-devops@users.noreply.github.com>
2020-08-03 14:03:05 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 80
 
-app.get('/', (req, res) => res.send('Lets see if this works after removing the Secrets from Github'))
+app.get('/', (req, res) => res.send('Retesting the pipeline after the rebuild!'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
