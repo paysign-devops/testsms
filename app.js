aaf7ea222b791e2f5191308fb5ee0e4b6ab7901e
Paysign_DevOps <68657347+paysign-devops@users.noreply.github.com>
2020-08-03 14:24:01 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 80
 
-app.get('/', (req, res) => res.send('Retesting the pipeline after the rebuild!'))
+app.get('/', (req, res) => res.send('If you can read this, IT WORKED!'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
